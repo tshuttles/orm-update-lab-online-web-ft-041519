940016bfcbad286b4796bf02e14a5d213b757d76
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-24 16:08:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,13 +13,17 @@ class Student
   
   def self.create_table 
     sql = <<-SQL
-      CREATE TABLE students 
-      
-    SQL
-    
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
   end 
   
   def self.drop_table 
+    
   end 
   
   def save 
