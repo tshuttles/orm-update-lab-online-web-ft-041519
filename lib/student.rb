bc110f3373717d0086726f1845b282676c961c95
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-24 16:39:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,6 +39,17 @@ class Student
     #   DB[:conn].execute(sql, self.name, self.grade)
     #   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     # end 
+    if self.id
+      self.update
+    else
+      sql = <<-SQL 
+        INSERT INTO students (name, grade)
+        VALUES (?,?)
+      SQL
+
+        DB[:conn].execute(sql, self.name, self.grade)
+       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
   end 
   
   def self.create 
