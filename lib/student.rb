c73f45e8e37e103b97043749f853ba4d075b6d05
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-24 16:45:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,7 +48,11 @@ class Student
   end 
   
   def self.new_from_db 
-    
+    new_student = self.new 
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.grade = row[2]
+    new_student
   end 
   
   def self.find_by_name 
